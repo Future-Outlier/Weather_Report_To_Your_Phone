643e779a5a94132dd3b8adf8d21eb1bab0b26e04
Slashie_person_and_his_Computer_Science_life <76461262+Hacker-Davinci@users.noreply.github.com>
2021-06-01 00:26:17 +0800

    Update json_in_script_tags.py

--- Weather_Report_To_Your_Phone/json_in_script_tags.py
+++ Weather_Report_To_Your_Phone/json_in_script_tags.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-AC3118BE-7C3D-481E-A522-113DA465F02F&format=JSON&locationName=%E5%98%89%E7%BE%A9%E7%B8%A3"
+url = "You have to google for the api from Central Weather Bureau"
 
 r = requests.get(url)
 print(r.status_code) # if 200 means ok from HTTP Response
